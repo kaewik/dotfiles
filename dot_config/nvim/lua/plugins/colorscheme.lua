6ffd1a0dcea8bbb085131a8993d1a882c67859b5
Lukas Jonathan Zenner <lukas.jonathan.zenner@web-computing.de>
2026-01-28 11:31:41 +0100

    Use github theme

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,9 @@
 return {
+  { 'projekt0n/github-nvim-theme', name = 'github-theme' },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-day",
+      colorscheme = "github_light",
     },
   },
 }
